876b238d0d171732e7afe81bc44ae4b70e644b23
Shob3r <andrewpinter23@gmail.com>
2024-10-21 14:40:49 -0700

    Add player zoom function with clamping and everything, I am such a good C# developer

--- Assets/Scripts/Input/PlayerCamera.cs
+++ Assets/Scripts/Input/PlayerCamera.cs
@@ -3,14 +3,17 @@ using UnityEngine;
 public class PlayerCamera : MonoBehaviour
 {
     private Transform playerModel;
-    public float maximumAngle, minimumAngle;
+    private Camera playerCamera;
+    public float zoomFactor;
+    public float maximumFieldOfView = 150;
+    public float minimumFieldOfView = 60;
     
     private float mouseSensitivity = 5f;
     private PlayerInput playerInput;
     private void Start()
     {
         playerModel = GameObject.FindGameObjectWithTag("Player").transform;
-        
+        playerCamera = playerModel.gameObject.GetComponentInChildren<Camera>();
         playerInput = new PlayerInput();
         playerInput.Enable();
     }
@@ -19,14 +22,24 @@ public class PlayerCamera : MonoBehaviour
     {
         if (Time.timeScale != 0)
         {
-            onPlayerLook(playerInput.Game.Look.ReadValue<Vector2>());
+            Vector2 playerLook = playerInput.Game.Look.ReadValue<Vector2>();
+            if(playerLook != Vector2.zero) onPlayerLook(playerLook);
+
+            Vector2 cameraZoom = playerInput.Game.Zoom.ReadValue<Vector2>();
+            float zoomAmount = -cameraZoom.y / zoomFactor;
+            
+            if(zoomAmount != 0) zoomCamera(zoomAmount);
         }
     }
 
 
     private void onPlayerLook(Vector2 mouseDelta)
     {
-        float cameraX = mouseDelta.x * mouseSensitivity * Time.deltaTime;
-        float cameraY = -mouseDelta.y * mouseSensitivity * Time.deltaTime;
+        
+    }
+
+    private void zoomCamera(float zoomDelta)
+    {
+        playerCamera.fieldOfView = Mathf.Clamp(playerCamera.fieldOfView + zoomDelta, minimumFieldOfView, maximumFieldOfView);
     }
 }
