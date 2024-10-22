957b3afa60b4efa60fcdbf63106201adfdd32d07
Shob3r <andrewpinter23@gmail.com>
2024-10-21 22:42:28 -0700

    Add smooth player camera zoom i am so peak guys

--- Assets/Scripts/Input/PlayerCamera.cs
+++ Assets/Scripts/Input/PlayerCamera.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
@@ -5,8 +6,10 @@ public class PlayerCamera : MonoBehaviour
     public float zoomFactor;
     
     private Camera playerCamera;
+    private Coroutine zoomRoutine;
     private PlayerInput playerInput;
     private Transform playerModel;
+    
     
     private readonly float[] fieldOfViewClamp = { 60, 150 };
     
@@ -27,7 +30,7 @@ public class PlayerCamera : MonoBehaviour
             
             // Use negative value to make the scroll/zoom direction correct
             float zoomAmount = -playerInput.Game.Zoom.ReadValue<Vector2>().y / zoomFactor; 
-            if(zoomAmount != 0) zoomCamera(zoomAmount);
+            if(zoomAmount != 0) zoomCameraHelper(zoomAmount);
         }
     }
 
@@ -37,9 +40,29 @@ public class PlayerCamera : MonoBehaviour
         
     }
 
-    private void zoomCamera(float zoomDelta)
+    private void zoomCameraHelper(float zoomDelta)
     {
-        // TODO: Make zoom smooth
-        playerCamera.fieldOfView = Mathf.Clamp(playerCamera.fieldOfView + zoomDelta, fieldOfViewClamp[0], fieldOfViewClamp[1]);
+        float fovTarget = Mathf.Clamp(playerCamera.fieldOfView + zoomDelta, fieldOfViewClamp[0], fieldOfViewClamp[1]);
+        if (zoomRoutine != null)
+        {
+            StopCoroutine(zoomRoutine);
+        }
+        zoomRoutine = StartCoroutine(cameraZoomSmooth(fovTarget));
+    }
+
+    private IEnumerator cameraZoomSmooth(float fovTarget, float duration = 0.2f)
+    {
+        float startFov = playerCamera.fieldOfView;
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = elapsed / duration;
+            // Using SmoothStep for a smoother transition
+            float currentFieldOfView = Mathf.Lerp(startFov, fovTarget, Mathf.SmoothStep(0f, 1f, t));
+            playerCamera.fieldOfView = currentFieldOfView;
+            yield return null;
+        }
     }
 }
