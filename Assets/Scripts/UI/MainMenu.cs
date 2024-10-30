9b4fa22e5cc7e37d925eb718519e1074a14ea79d
Shob3r <andrewpinter23@gmail.com>
2024-10-30 10:07:40 -0700

    more ui work

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,18 +1,43 @@
 using System;
 using System.Collections;
+using LemonStudios.UI;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public float menuSelectorMovetime = 0.3f;
+
+    public AudioClip menuSelectionSound;
+
+    private AudioSource mainMenuAudioSource;
     private Coroutine selectorCoroutine;
     private Image buttonSelector;
+    private TextMeshProUGUI versionText;
 
     private void Start()
     {
         buttonSelector = GameObject.FindGameObjectWithTag("MenuSelector").GetComponent<Image>();
-    }
+        versionText = GameObject.FindGameObjectWithTag("GameVersionText").GetComponent<TextMeshProUGUI>();
+        mainMenuAudioSource = GetComponentInParent<AudioSource>();
 
+
+        versionText.text = $"Version {Application.version}";
+    }
+    /* Menu Button methods */
+
+    public void switchMenus()
+    {
+        LemonUIUtils.SwitchMenus();
+    }
+    
+    public void openGithubLink()
+    {
+        Application.OpenURL("https://github.com/lemons-studios/out-of-body");
+    }
+    
+    /* Menu Selector methods*/
     public void moveMenuSelectorHelper(float yPos)
     {
         if (selectorCoroutine != null)
@@ -20,14 +45,13 @@ public class MainMenu : MonoBehaviour
             StopCoroutine(selectorCoroutine);
             selectorCoroutine = null;
         }
-
-        selectorCoroutine = StartCoroutine(moveMenuSelector(yPos, 0.75f));
+        mainMenuAudioSource.PlayOneShot(menuSelectionSound);
+        selectorCoroutine = StartCoroutine(moveMenuSelector(yPos, menuSelectorMovetime));
     }
 
-    private IEnumerator moveMenuSelector(float targetPosition, float time = 0.4f)
+    private IEnumerator moveMenuSelector(float targetPosition, float time)
     {
         float elapsed = 0f;
-
         while (elapsed < time)
         {
             elapsed += Time.deltaTime;
@@ -38,6 +62,7 @@ public class MainMenu : MonoBehaviour
             buttonSelector.transform.localPosition = new Vector3(buttonSelector.transform.localPosition.x, intermediateYPos, 0);
             yield return null;
         }
+        
         buttonSelector.transform.localPosition = new Vector3(buttonSelector.transform.localPosition.x, targetPosition, 0);
     }
 }
