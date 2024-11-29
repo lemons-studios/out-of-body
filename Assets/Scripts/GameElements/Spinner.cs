2b998ff2691cbbde5758dc2a0a7c0128864d9b69
Shob3r <andrewpinter23@gmail.com>
2024-11-29 11:13:51 -0800

    randomize spinner rotation, some more scene work We're finally getting somewhere frfrfrfr

--- Assets/Scripts/GameElements/Spinner.cs
+++ Assets/Scripts/GameElements/Spinner.cs
@@ -1,23 +1,33 @@
 using UnityEngine;
 public class Spinner : MonoBehaviour
 {
-    [Tooltip("Degrees of rotation per second. Can be a negative value. Cannot be zero")]
-    public float rotationSpeed = 5;
-
+    [Tooltip("Degrees of rotation per second. Can be a negative value. Cannot be zero")] [SerializeField] private float minRotationSpeed = 10; 
+    [Tooltip("Degrees of rotation per second. Can be a negative value. Cannot be zero")] [SerializeField] private float maxRotationSpeed = 35;
+    [SerializeField] private float speedMultiplier = 3.5f; // Probably a temporary thing (lmao)
+    
     private Rigidbody spinnerRb;
     private Vector3 rotationVector;
     private bool playerOnPlatform;
+    private float trueRotationSpeed;
     
     private void Start()
     {
-        rotationSpeed = Mathf.Max(rotationSpeed, 1);
-        rotationVector = new Vector3(0, rotationSpeed, 0);
+        if (minRotationSpeed == 0) minRotationSpeed = 10; // Reset to default
+        if (maxRotationSpeed == 0) maxRotationSpeed = 35; // Again, reset to default
+        
+        trueRotationSpeed = Random.Range(minRotationSpeed, maxRotationSpeed);
+        rotationVector = new Vector3(0, trueRotationSpeed, 0);
         spinnerRb = GetComponentInChildren<Rigidbody>();
+        
+        // I do not like working with rotation math
+        Vector3 eulerAngles = spinnerRb.gameObject.transform.rotation.eulerAngles;
+        eulerAngles.y = Random.Range(-180f, 360f); // f lets the compiler know that floating point numbers are accepted; otherwise they would only pick full numbers between this range
+        spinnerRb.gameObject.transform.rotation = Quaternion.Euler(eulerAngles);
     }
 
     private void FixedUpdate()
     {
-        Quaternion deltaRotation = Quaternion.Euler(rotationVector * Time.fixedDeltaTime);
+        Quaternion deltaRotation = Quaternion.Euler(rotationVector * (Time.fixedDeltaTime * speedMultiplier));
         spinnerRb.MoveRotation(spinnerRb.transform.rotation * deltaRotation);
     }
 }
