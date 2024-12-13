e4b7c5b29d94814962caca5c2ea5ffe55e3ee6bc
Shob3r <andrewpinter23@gmail.com>
2024-12-13 14:44:06 -0800

    level 1 probably done :sunglasses:

--- Assets/Scripts/GameElements/Checkpoint.cs
+++ Assets/Scripts/GameElements/Checkpoint.cs
@@ -1,22 +1,27 @@
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(Collider))]
 public class Checkpoint : MonoBehaviour
 {
     public int checkpointIndex;
     
     private CheckpointAndAttempts sceneCheckpoints;
+    [SerializeField] private AudioClip checkpointReachedSoundEffect;
     private bool isFirstTrigger = true;
     
     private void Start()
     {
         sceneCheckpoints = GameObject.FindGameObjectWithTag("GameController").GetComponent<CheckpointAndAttempts>();
-        if(!GetComponent<BoxCollider>().isTrigger) Debug.LogError($"{gameObject.name}'s trigger is not enabled. please enable it in order for it's checkpoint to work properly");
+        if(!GetComponent<Collider>().isTrigger) Debug.LogError($"{gameObject.name}'s trigger is not enabled. please enable it in order for it's checkpoint to work properly");
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (isFirstTrigger && other.gameObject.CompareTag("Player")) { sceneCheckpoints.SetNewCheckpoint(checkpointIndex); }
+        if (isFirstTrigger && other.gameObject.CompareTag("Player"))
+        {
+            sceneCheckpoints.SetNewCheckpoint(checkpointIndex);
+            GameObject.FindGameObjectWithTag("GlobalSfx").GetComponent<AudioSource>().PlayOneShot(checkpointReachedSoundEffect);
+        }
         isFirstTrigger = false;
     }
 }
--- Assets/Scripts/GameElements/LevelHazard.cs
+++ Assets/Scripts/GameElements/LevelHazard.cs
@@ -6,8 +6,8 @@ public class LevelHazard : MonoBehaviour
     private AudioSource sfxAudioSource;
 
     private void Start() { sfxAudioSource = GameObject.FindGameObjectWithTag("GlobalSfx").GetComponent<AudioSource>(); }
-
-    private void OnCollisionEnter(Collision other)
+    
+    private void OnTriggerEnter(Collider other)
     {
         if (!other.gameObject.CompareTag("Player")) return;
         other.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero; // Prevent scenarios where the player is moving so fast that movement calculations do not know where to place it, and it ends up clipping through the entire level
